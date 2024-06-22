ff6d17399560acccaea24e2662d2a1d01c49017b
d-yerovi <day5@njit.edu>
2024-06-22 15:50:51 -0400

    fixing some pytest issues

--- tests/test_app.py
+++ tests/test_app.py
@@ -26,7 +26,6 @@ def test_app_start_unknown_command(capfd, monkeypatch):
     out, err = capfd.readouterr()
     if err:
         print("Error:", err)
-    
     # Check that the REPL responds to an unknown command and then exits after 'exit' command
     assert "Hello World. Type 'exit' to exit." in out
     assert "Available commands:" in out
